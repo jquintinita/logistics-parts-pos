1632f4841689c24246b36c77bcd6448ff58e7d59
jquintinita <joseph.quintinita@gmail.com>
2025-08-05 23:19:45 +0800

    Fix issue

--- src/Layouts/DashboardLayout.jsx
+++ src/Layouts/DashboardLayout.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Outlet } from 'react-router-dom';
 import SideMenuBar from '../component/layout/SideMenuBar';
-import Topbar from '../component/layout/topbar';
+import Topbar from '../component/layout/Topbar';
 import TopSlider from '../component/widget/TopSlider';
 import MobileSlider from '../component/widget/MobileSlider';
 
